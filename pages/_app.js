01eb38721f7a88042f70ac1e4a30e541362cef07
Kekzploit <kekzploit@mailfence.com>
2023-04-08 18:51:36 +0100

    added plausible analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 import "@/styles/globals.css";
 import "react-responsive-modal/styles.css";
 
@@ -7,10 +9,18 @@ import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
     return (
-        <ContextProvider>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </ContextProvider>
+        <>
+            <Script
+                defer
+                data-domain="zanobazaar.com"
+                src="https://plausible.io/js/script.js"
+            />
+
+            <ContextProvider>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </ContextProvider>
+        </>
     );
 }
